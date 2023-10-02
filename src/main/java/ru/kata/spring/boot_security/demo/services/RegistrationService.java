bcd4aa9a8491f87171d47c55b2deac13c6832f42
Sergei Kadushkin <blackoverlord.inc@mail.ru>
2023-10-02 18:57:38 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RegistrationService.java
@@ -0,0 +1,22 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.models.Person;
+import ru.kata.spring.boot_security.demo.repositories.PeopleRepository;
+
+@Service
+public class RegistrationService {
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public RegistrationService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Transactional
+    public void register(Person person){
+        peopleRepository.save(person);
+    }
+}
